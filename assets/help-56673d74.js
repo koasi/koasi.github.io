05194e44c5b18d5e5181f0abecc7ea2803e25d36
IAN_NB <ianvix.tw@gmail.com>
2025-02-16 00:42:28 +0800

    fix_serialport

---
+++ assets/help-56673d74.js
@@ -0,0 +1,1 @@
+import{G as e,$ as n,c as a,T as o}from"./DarkTheme-6565bed1.js";const i={};i.initialize=function(t){e.active_tab!="help"&&(e.active_tab="help"),n("#content").load("./tabs/help.html",function(){a.localizePage(),e.content_ready(t)})};i.cleanup=function(t){t&&t()};o.help=i;export{i as help};
