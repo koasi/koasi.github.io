dcca8cd3c07adff26c9061859349b28af5b359df
IAN_NB <ianvix.tw@gmail.com>
2025-07-02 03:14:37 +0800

    clean

--- assets/help-bcb2c892.js
+++
@@ -1,1 +0,0 @@
-import{G as e,$ as n,c as a,T as o}from"./DarkTheme-59411532.js";const i={};i.initialize=function(t){e.active_tab!="help"&&(e.active_tab="help"),n("#content").load("./tabs/help.html",function(){a.localizePage(),e.content_ready(t)})};i.cleanup=function(t){t&&t()};o.help=i;export{i as help};
